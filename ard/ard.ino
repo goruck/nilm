d3bccbc4c8cea5e0405c3f7e1c7a99023deb675c
goruck <lindo.st.angel@gmail.com>
2022-02-24 18:02:44 -0800

    changes to increase serial read speed

--- ard/ard.ino
+++ ard/ard.ino
@@ -1,5 +1,13 @@
 /*
 
+Continually sends the following over the serial port:
+  RMS line voltage (one phase only)
+  RMS current (both phases)
+  Calculated real power (both phases)
+  Calcuated apparent power (both phases).
+
+Copyright (c) 2022 Lindo St. Angel
+
 */
 
 #include <Arduino.h>
@@ -8,27 +16,20 @@
 void setup() 
 {
 
-  //Serial.begin(9600);
-  //Serial.println("Set baud=115200");
-  //Serial.end();
   Serial.begin(115200);
+
+  EmonLibCM_SetADC_VChannel(0, 132.0);         // ADC Input channel, voltage calibration
+  EmonLibCM_SetADC_IChannel(1, 58.0, 4.6);     // ADC Input channel, current calibration, phase calibration
+  EmonLibCM_SetADC_IChannel(2, 58.0, 4.2);     // The current channels will be read in this order
+
+  EmonLibCM_setADC_VRef(INTERNAL2V56);         // ADC Reference voltage (set to 2.56V)
+  EmonLibCM_ADCCal(2.56);                      // ADC Cal voltage (set to 2.56V)
   
-  Serial.println("\nStaring monitoring...");
+  EmonLibCM_cycles_per_second(60);             // Line frequency (set to 60 Hz)
 
-  EmonLibCM_SetADC_VChannel(0, 132.0);                    // ADC Input channel, voltage calibration
-  EmonLibCM_SetADC_IChannel(1, 58.0, 4.6);                // ADC Input channel, current calibration, phase calibration
-  EmonLibCM_SetADC_IChannel(2, 58.0, 4.2);                // The current channels will be read in this order
+  EmonLibCM_datalog_period(0.1);               // Interval over which stats are reported (in secs)
 
-  EmonLibCM_setADC_VRef(INTERNAL2V56);                    // ADC Reference voltage (set to 2.56V)
-  EmonLibCM_ADCCal(2.56);                                 // ADC Cal voltage (set to 2.56V)
-  
-  EmonLibCM_cycles_per_second(60);                        // Line frequency is 60Hz
-  
-  EmonLibCM_min_startup_cycles(10);                       // Number of cycles to let ADC run before starting first actual measurement
-
-  EmonLibCM_datalog_period(10.0);                         // Set interval over which stats are reports
-
-  EmonLibCM_Init();                                       // Start continuous monitoring
+  EmonLibCM_Init();                            // Start continuous monitoring
 
 }
 
@@ -38,36 +39,17 @@ void loop()
   if (EmonLibCM_Ready())
   {
     
-    Serial.println(EmonLibCM_getVrms());
-    Serial.println(EmonLibCM_getLineFrequency(),2);
-    for (byte ch=0; ch<2; ch++)
-    {
-        Serial.println(EmonLibCM_getIrms(ch),3);
-        Serial.println(EmonLibCM_getRealPower(ch));
-        Serial.println(EmonLibCM_getApparentPower(ch));
-        Serial.println(EmonLibCM_getWattHour(ch));
-        Serial.println(EmonLibCM_getPF(ch),4);
-        delay(10);
-    }
+    Serial.print(EmonLibCM_getVrms());Serial.print(",");
 
-    /*
-    Serial.println(EmonLibCM_acPresent()?"AC present ":"AC missing ");
-    delay(5);
+    Serial.print(EmonLibCM_getIrms(0),3);Serial.print(",");
+    Serial.print(EmonLibCM_getRealPower(0));Serial.print(",");
+    Serial.print(EmonLibCM_getApparentPower(0));Serial.print(",");
+    Serial.print(EmonLibCM_getIrms(1),3);Serial.print(",");
+    Serial.print(EmonLibCM_getRealPower(1));Serial.print(",");
+    Serial.print(EmonLibCM_getApparentPower(1));
 
-    Serial.print(" V=");Serial.print(EmonLibCM_getVrms());
-    Serial.print(" f=");Serial.println(EmonLibCM_getLineFrequency(),2);
+    Serial.println(); // Completes one sample.
 
-    for (byte ch=0; ch<2; ch++)
-    {
-        Serial.print("Ch ");Serial.print(ch+1);
-        Serial.print(" I=");Serial.print(EmonLibCM_getIrms(ch),3);
-        Serial.print(" W=");Serial.print(EmonLibCM_getRealPower(ch));
-        Serial.print(" VA=");Serial.print(EmonLibCM_getApparentPower(ch));
-        Serial.print(" Wh=");Serial.print(EmonLibCM_getWattHour(ch));
-        Serial.print(" pf=");Serial.print(EmonLibCM_getPF(ch),4);
-        Serial.println();
-        delay(10);
-    }
-    */
   }
+
 }
